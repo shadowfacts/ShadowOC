d1589e9051414a2308d7d909a317176c156f2c38
Shadowfacts <shadowfacts@users.noreply.github.com>
2015-12-24 21:00:16 -0500

    Create barrels.lua

---
+++ programs/btm16/barrels.lua
@@ -0,0 +1,27 @@
+local component = require("component")
+
+local barrels = {}
+local emitters = {}
+
+local i = 0
+local j = 0
+for k,v in component.list() do
+	if v == "yot_barrel" then
+		barrels[i] = component.get(v)
+		i = i + 1
+	elseif v == "redstone" then
+		emitters[j] = component.get(v)
+		j = j + 1
+	end
+end
+
+while true do
+	for i=0,2 do
+		if barrels[i].getFluidAmount() >= 40000 then
+			emitters[i].setOutput(south, 15)
+		else
+			emitters[i].setOutput(south, 0)
+		end
+	end
+	os.sleep(2)
+end
